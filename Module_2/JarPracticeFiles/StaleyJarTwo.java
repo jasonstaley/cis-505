588144c6e22f638565352beb75d657f8648f8b04
jasonstaley <jestaley@my365.bellevue.edu>
2021-09-09 16:25:07 -0500

    Jason Staley - Assignment 2.2

---
+++ Module_2/JarPracticeFiles/StaleyJarTwo.java
@@ -0,0 +1,17 @@
+/*
+Student:	Jason Staley
+Date: 		9/9/2021
+Assignment: 2.2 - Jar Files
+File Name: 	StaleyJarTwo.java
+
+University:	Bellevue University
+Class:		CIS505-T301 Intermediate Java Programming (2221-1)
+Professor: 	Michael McGee
+*/
+
+public class StaleyJarTwo {
+    public static void main(String[] args) {
+	//Practice file #2
+	System.out.printf("Practice 2");
+    }
+}
